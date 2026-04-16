b49a2df259bf5b22178579f79938565e4a2462f2
Fernando Jesus Serrano Garrido <2213010687@untels.edu.pe>
2026-04-16 10:15:47 -0500

    Flujo actualizado con acceso a clínica galenius y modulización inicada de directorios

--- sigemo_galenius/selectors.py
+++
@@ -1,7 +0,0 @@
-SEL = {
-    "login_form": "form#loginform",
-    "csrf": 'input[name="csrfmiddlewaretoken"]',
-    "username": "#username",
-    "password": "#password",
-    "submit": "#wp-submit",
-}
